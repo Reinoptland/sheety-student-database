283d319854d34da5ab5bd7395c43d1dddc012013
Reinoptland <reinoptland@gmail.com>
2021-05-11 15:37:43 +0200

    Check which requests come first

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ const axios = require("axios")
 // Promise { <pending> }
 
 async function getCrypto(){
+    console.log("LETS MAKE A REQUEST")
     const response = await axios.get("https://api.coincap.io/v2/assets")
-    console.log(response) // wat krijgen we terug??
+    console.log(response.status) // wat krijgen we terug??
+    console.log("REQUEST IS DONE")
 }
 
+console.log("HALLO STUDENTS")
+
 getCrypto()
+
+console.log("CRYPTOS ARE COOL")
+
+// Voorspellingen:
+// Hallo students, lets make a request, status, request is done, cryptos are coool
+// Hallo students, lets make a request, cryptos are cool, request is done
